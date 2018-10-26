262ef758228a166f34a0ec0b24f04b4c5d16408c
Das <290692@cognizant.com>
2018-10-26 18:29:46 +0530

    first commit

---
+++ controllers/document.controller.js
@@ -0,0 +1,41 @@
+﻿var config = require('config.json');
+var express = require('express');
+var router = express.Router();
+var multer = require('multer');
+var documentService = require('services/document.service');
+const multerConfig = {
+    
+    storage: multer.diskStorage({
+     //Setup where the user's file will go
+     destination: function(req, file, next){
+       next(null, './uploads');
+       },   
+        
+        //Then give the file a unique name
+        filename: function(req, file, next){
+            console.log(file);
+            const ext = file.mimetype.split('/')[1];
+            next(null, file.fieldname + '-' + Date.now() + '.'+ext);
+          }
+        })
+      };
+
+var uploadDoc = multer({dest:'./uploads'}); 
+// routes
+router.post('/upload', multer(multerConfig).single('fileName'), upload);
+
+module.exports = router;
+
+
+function upload(req, res) {
+    console.log(req.fileName, req.description);
+    res.send('Complete!');
+    // fro data insert to dbb
+    /* documentService.create(req)
+        .then(function () {
+            res.json('success');
+        })
+        .catch(function (err) {
+            res.status(400).send(err);
+        }); */
+}
---
+++ server.js
@@ -0,0 +1,36 @@
+﻿require('rootpath')();
+var express = require('express');
+var app = express();
+var cors = require('cors');
+var bodyParser = require('body-parser');
+var expressJwt = require('express-jwt');
+var config = require('config.json');
+
+var corsOptions = {
+    origin: '*',
+    optionsSuccessStatus: 200
+};
+
+app.use(cors(corsOptions));
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+});
+
+
+// routes
+app.use('/users', require('./controllers/users.controller'));
+app.use('/documents', require('./controllers/document.controller'));
+// error handler
+app.use(function (err, req, res, next) {
+    throw err;
+});
+
+// start server
+var port = process.env.NODE_ENV === 'production' ? 80 : 4000;
+var server = app.listen(port, function () {
+    console.log('Server listening on port ' + port);
+});
